9e2dad1d0b1742ad224f34aa838d487c8ea692be
Khalood.kk <alnasseikallood@gmail.com>
2026-01-04 09:15:24 +0400

    adding task2

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@
                 Console.WriteLine("Welcome to system");
                 Console.WriteLine("Select the process you need: ");
                 Console.WriteLine(" 1.Calculate the average of five numbers.");
+                Console.WriteLine(" 2.Check whether a number is even or odd.");
                 
                 int option = int.Parse(Console.ReadLine());
 
@@ -38,11 +39,24 @@
                         Console.WriteLine("The avrage is :" + avg);
                         break;
 
+                        case 2:
+                        Console.WriteLine("Enter The Number :");
+                        float number = float.Parse(Console.ReadLine());
 
-                      
+                        if (number % 2 == 0)
+                        {
+                            Console.WriteLine("even");
+                        }
+                        else
+                        {
+                            Console.WriteLine("odd");
+                        }
+                        break;
 
-                        
-                       
+
+
+
+
 
 
 
